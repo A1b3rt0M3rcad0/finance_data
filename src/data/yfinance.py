5864eb3b9fbaaf56e1852f9aa5f69f2b988a99a2
Alberto S. Mercado <109180196+A1b3rt0M3rcad0@users.noreply.github.com>
2025-04-27 18:57:20 -0300

    refact: coluna de tempo adicionada

--- src/data/yfinance.py
+++ src/data/yfinance.py
@@ -9,4 +9,5 @@ class Stock:
     def get_price(self, period:str, interval:str) -> Dict:
         df = self.stock.history(period=period, interval=interval)
         df.index = df.index.strftime('%Y-%m-%d %H:%M:%S')
+        df["Time"] = df.index
         return df.to_dict()
